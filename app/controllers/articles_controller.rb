fd4159467d52aaf6c72f91fc20b1562e9fc9ada7
ricardo39985 <ricardo39985@gmail.com>
2020-02-29 00:43:03 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,20 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+  def edit
+    @article=Article.find(params[:id])    
+  end
 
+  def update
+ 
+    @article=Article.find(params[:id])
+    @article.update(title: params[:article][:title]) if !params[:article][:title].empty?
+    @article.update(title: params[:article][:description]) if !params[:article][:description]
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
   # add edit and update methods here
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  get '/articles/:id/edit', to: 'articles#edit', as: 'article_edit'
+  get 'articles/:id', to: 'articles#show'
+  patch 'articles/:id', to: 'articles#update'
 end
